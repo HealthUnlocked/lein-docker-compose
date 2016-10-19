7c54c5a848ad51b44accf5039e8b084d091c4a3f
Russell <russell@russelldunphy.com>
2016-10-19 16:51:57 +0100

    :smiley_cat: release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject healthunlocked/lein-docker-compose "0.1.0-SNAPSHOT"
+(defproject healthunlocked/lein-docker-compose "0.1.0"
   :description "Leiningen plugin that provides environ with docker-compose port mappings"
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
