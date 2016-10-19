50823b9966648ea9387b1eaa3a421761a986b099
Russell <russell@russelldunphy.com>
2016-10-19 19:56:03 +0100

    :older_woman: lein ancient

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.forward/yaml "1.0.4"]
+  :dependencies [[io.forward/yaml "1.0.5"]
                  [lein-environ "1.1.0"]]
   :profiles {:dev {:resource-paths ["test/resources"]
-                   :dependencies [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-                                  [lein-ancient "0.6.10"]]}}
+                   :dependencies [[lein-ancient "0.6.10"]]}}
   :deploy-repositories [["clojars" {:username :env/clojars_username
                                     :password :env/clojars_password
                                     :url "https://clojars.org/repo/"}]]
