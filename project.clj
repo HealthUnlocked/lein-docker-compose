8c7d9b31e230da42e8380981568651820902ecfd
Russell <russell@russelldunphy.com>
2017-03-03 12:07:56 +0000

    :construction_worker: bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject healthunlocked/lein-docker-compose "0.1.1-SNAPSHOT"
+(defproject healthunlocked/lein-docker-compose "0.1.1"
   :description "Leiningen plugin that provides environ with docker-compose port mappings"
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
