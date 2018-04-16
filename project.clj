5386c2e30a95eb2b80dfeab415fb5af4c83fbb85
Mahsa Mohammadkhani <Mahsa.mohammadkhani@healthunlocked.com>
2018-04-16 11:01:10 +0100

    fix dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject healthunlocked/lein-docker-compose "0.1.2"
+(defproject healthunlocked/lein-docker-compose "0.1.3"
   :description "Leiningen plugin that provides environ with docker-compose port mappings"
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.forward/yaml "1.0.5"]
+  :dependencies [[io.forward/yaml "1.0.7"]
                  [lein-environ "1.1.0"]]
   :profiles {:dev {:resource-paths ["test/resources"]
-                   :dependencies [[lein-ancient "0.6.10"]]}}
+                   :dependencies [[lein-ancient "0.6.15"]]}}
   :deploy-repositories [["clojars" {:username :env/clojars_username
                                     :password :env/clojars_password
                                     :url "https://clojars.org/repo/"}]]
