0dcf77b8d28aead69ece9c4f8745b375b0a3c75f
Russell Dunphy <russell@russelldunphy.com>
2018-09-07 19:32:39 +0100

    releas

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject healthunlocked/lein-docker-compose "0.2.0-SNAPSHOT"
+(defproject healthunlocked/lein-docker-compose "0.2.0"
   :description "Leiningen plugin that provides environ with docker-compose port mappings"
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
@@ -9,5 +9,6 @@
                    :dependencies [[lein-ancient "0.6.15"]]}}
   :deploy-repositories [["clojars" {:username :env/clojars_username
                                     :password :env/clojars_password
+                                    :sign-releases false
                                     :url "https://clojars.org/repo/"}]]
   :eval-in-leiningen true)
