569cd3a7a9d8ebc66cb696e6bf06e82c97d793bb
Alex Trofymenko <byte.shaggy@gmail.com>
2019-11-20 16:26:55 +0000

    Update dependency to resolve https://github.com/clj-commons/ordered/issues/40

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject healthunlocked/lein-docker-compose "0.2.0"
+(defproject healthunlocked/lein-docker-compose "0.2.1"
   :description "Leiningen plugin that provides environ with docker-compose port mappings"
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.forward/yaml "1.0.7"]
+  :dependencies [[io.forward/yaml "1.0.9"]
                  [lein-environ "1.1.0"]]
   :profiles {:dev {:resource-paths ["test/resources"]
                    :dependencies [[lein-ancient "0.6.15"]]}}
