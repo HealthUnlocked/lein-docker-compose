fae239d8af55d8b1d855e5744499a653b284d072
Alex Trofymenko <byte.shaggy@gmail.com>
2019-11-20 16:26:55 +0000

    Update dependency to resolve https://github.com/clj-commons/ordered/issues/40

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/healthunlocked/lein-docker-compose"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.forward/yaml "1.0.9"]
+  :dependencies [[org.flatland/ordered "1.5.7"]
+                 [io.forward/yaml "1.0.9" :exclusions [org.flatland/ordered]]
                  [lein-environ "1.1.0"]]
   :profiles {:dev {:resource-paths ["test/resources"]
                    :dependencies [[lein-ancient "0.6.15"]]}}
